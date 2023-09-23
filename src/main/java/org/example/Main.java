8083043b3d1ac930aec0e14a58b77bfddafabc22
fangjjcs <grace963214789@gmail.com>
2023-09-23 16:13:11 +0800

    file and path

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,41 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Hello and welcome!");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String filename = "C:\\Users\\fang\\Documents\\repo\\01\\test.txt";
+        testFile(filename);
+        File file = new File(filename);
+        // Look Before You Leap
+        if(!file.exists()) {
+            System.out.println("Can not find this file.");
+            return;
+        }
+        System.out.println("We find the file, good to go.");
+
+    }
+
+    private static void testFile(String filename) {
+        Path path = Paths.get(filename);
+        try {
+            List<String> lines = Files.readAllLines(path);
+            System.out.println("File exists and able to use as a resource.");
+            System.out.println(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("Need to log something.");
         }
     }
 }

c01ea62c0179859334af45191578367378dda120
fangjjcs <grace963214789@gmail.com>
2023-10-09 21:39:50 +0800

    add download function

--- src/main/java/org/example/FileUpload.java
+++ src/main/java/org/example/FileUpload.java
@@ -1,9 +1,7 @@
 package org.example;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.UploadObjectArgs;
-import io.minio.errors.MinioException;
+import io.minio.*;
+import io.minio.errors.*;
+import io.minio.messages.Item;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -23,7 +21,7 @@ import java.util.stream.Stream;
 
 public class FileUpload {
     private static final Logger logger = LoggerFactory.getLogger(FileUpload.class);
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException, ParseException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
 
 
         logger.trace("Logger start.");
@@ -37,19 +35,18 @@ public class FileUpload {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
         Timestamp timeThreshold = new Timestamp (dateFormat.parse("23/09/2023 23:00:00").getTime());
 
-        uploadFile(path, trimPath, timeThreshold);
+        MinioClient minioClient = initMinioClient();
+
+        // Upload Files
+        uploadFile(minioClient, path, trimPath, timeThreshold);
         System.out.println("Upload Done.");
         logger.info("Upload Done.");
 
+        // Download Files
+        ListAndDownloadMinioFiles(minioClient);
     }
 
-    private static void uploadFile(Path path, String trimPath, Timestamp timeThreshold) {
-
-        MinioClient minioClient =
-                MinioClient.builder()
-                        .endpoint("https://play.min.io")
-                        .credentials("Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-                        .build();
+    private static void uploadFile(MinioClient minioClient, Path path, String trimPath, Timestamp timeThreshold) {
 
         try (Stream<Path> paths = Files.walk(path, 3)){
             paths.filter(Files::isRegularFile).forEach(p -> {
@@ -67,12 +64,12 @@ public class FileUpload {
                         System.out.println("Bucket 'testadam' doesn't exist.");
                     }
                     // Upload file as object name to bucket
-//                    minioClient.uploadObject(
-//                            UploadObjectArgs.builder()
-//                                    .bucket("testadam")
-//                                    .object(objectPath)
-//                                    .filename(filename)
-//                                    .build());
+                    minioClient.uploadObject(
+                            UploadObjectArgs.builder()
+                                    .bucket("testadam")
+                                    .object(objectPath)
+                                    .filename(filename)
+                                    .build());
                     System.out.println(
                             objectPath + " is successfully uploaded to bucket.");
                     System.out.println("----------------------------------------------");
@@ -90,6 +87,15 @@ public class FileUpload {
             throw new RuntimeException(e);
         }
     }
+
+    private static MinioClient initMinioClient() {
+        MinioClient minioClient =
+                MinioClient.builder()
+                        .endpoint("https://play.min.io")
+                        .credentials("Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
+                        .build();
+        return minioClient;
+    }
     private static void printFileInfo(Path path) {
         try {
             boolean isDir = Files.isDirectory(path);
@@ -101,5 +107,34 @@ public class FileUpload {
         } catch (IOException e) {
             System.out.println("Not a directory. Path : " + path);
         }
+    }
+
+    private static void ListAndDownloadMinioFiles(MinioClient minioClient) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        ListObjectsArgs args = ListObjectsArgs.builder()
+                .bucket("testadam")
+                .recursive(true)
+                .build();
+        Iterable<Result<Item>> response = minioClient.listObjects(args);
+
+        for (Result<Item> itemResult : response) {
+            Item item = itemResult.get();
+            System.out.println(item.objectName() + " | " + item.size() + " bytes");
+            downloadFiles(minioClient, item.objectName());
+        }
+    }
+
+    private static void downloadFiles(MinioClient minioClient, String objectName) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+        String filePath = "C:/Users/fang/Documents/download/" + objectName;
+
+        System.out.println(objectName);
+        System.out.println(filePath);
+
+        DownloadObjectArgs args = DownloadObjectArgs.builder()
+                .bucket("testadam")
+                .object(objectName)
+                .filename(filePath)
+                .build();
+        minioClient.downloadObject(args);
     }
 }
